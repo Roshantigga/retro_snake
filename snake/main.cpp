2c18bb5207986734b76ca11c428226f5a64305a3
Roshantigga <99roshantigga@gmail.com>
2025-07-31 23:02:28 +0530

    Creating a blank canvas and game loop  , 2nd commit

---
+++ snake/main.cpp
@@ -0,0 +1,58 @@
+
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+Color green = {173,204,96,255};
+Color darkGreen = {43,51,24,255};
+
+int cellSize = 30;
+int cellCount = 25;
+
+class Food {
+
+    public:
+    Vector2 position = {5,6};
+    Texture2D texture;
+
+    Food() {
+        Image image = LoadImage("Graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+    }
+
+    ~Food() 
+    {
+        UnloadTexture(texture);
+    }
+
+    void Draw()
+    {
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+    }
+};
+
+int main() 
+{
+    cout<<"Starting the game.. "<<endl;
+    InitWindow(cellSize*cellCount,cellSize*cellCount,"Retro snake");
+    SetTargetFPS(60);
+
+    Food food = Food();
+
+    while(WindowShouldClose() == false) //untill and unless the esc is pressed the game will not close , if esc pressed then it will return the true value
+    {
+        BeginDrawing();
+
+        //Drawing
+
+        ClearBackground(green);
+        food.Draw();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
